22702371a7c2797f769523c581fd3d07cb028519
vamprock19 <vamprock19@gmail.com>
2025-02-02 00:11:58 -0500

    cleaner code + input keyboard

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,35 +1,21 @@
 #include "ofApp.h"
+#include<array>;
 using namespace std;
 
 //--------------------------------------------------------------
 void ofApp::setup()
 {
+	ofSetFrameRate(60);
+	ofSetWindowTitle("Sorting Project");
 	srand(time(0));
 
-	
-	
+	array <int, 6> arrayList = { 0,0,0,0,0 }; /*5*/
 
-	for (int i = 0; i < 1; i++)
+	for (int i = 0; i < 5; i++)
 	{
-		array <int, 6> arrayList = { 0,0,0,0,0}; /*5*/
-
-		arrayList[0] = rand() % 10 + 1;
-		arrayList[1] = rand() % 10 + 1;
-		arrayList[2] = rand() % 10 + 1; 
-		arrayList[3] = rand() % 10 + 1;
-		arrayList[4] = rand() % 10 + 1;
-
-		cout << arrayList[0] << endl;
-		cout << arrayList[1] << endl;
-		cout << arrayList[2] << endl;
-		cout << arrayList[3] << endl;
-		cout << arrayList[5] << endl;
-
+		arrayList[i] = rand() % 100 + 10;/*between 100 and 10*/
+		cout << arrayList[i] << endl;
 	}
-	
-	
-
-	
 }
 
 //--------------------------------------------------------------
@@ -41,34 +27,22 @@ void ofApp::update()
 //--------------------------------------------------------------
 void ofApp::draw()
 {
-	ofSetColor(255, 255, 0);
-	ofDrawBitmapString("this is the homework!", 100, 100);
+	/*ofSetColor(255, 255, 0);
+	ofDrawBitmapString("this is the homework!", 100, 100);*/
 	ofSetColor(0, 0, 255);
 	bool isBlue = true;
-
+	int counter = 200;
 	srand(time(0));
+	array <int, 5> arrayList = { 0,0,0,0,0 }; /*5*/
 
 
-	for (int y = 200; y <= 200; y += 100)
+	for (int i = 0; i < 5; i++)
 	{
-		for()
-		array <int, 6> arrayList = { 0,0,0,0,0 }; /*5*/
-
-		arrayList[0] = rand() % 10 + 1;
-		arrayList[1] = rand() % 10 + 1;
-		arrayList[2] = rand() % 10 + 1;
-		arrayList[3] = rand() % 10 + 1;
-		arrayList[4] = rand() % 10 + 1;
-		
-		ofSetColor(255, 255, 255);
-		/*value x, value y, radius*/
-
-			ofDrawCircle(20, 20, 25); // radius
-		
+			arrayList[i] = rand() % 100 + 10; //  generates 5 random numbers
+			ofDrawCircle(200, counter, arrayList[i]);// radius
+			counter += 120;
 	}
-
-	
-
+		
 	/*for (int y = 200; y <= 400; y += 100)
 	{
 		for (int x = 100; x <= 500; x += 100)
@@ -92,13 +66,39 @@ void ofApp::draw()
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key)
 {
+	switch (key)
+	{
+	case 'b':
+		// bubble sort
+		break;
 
+	case'i':
+		// insertion sort
+		break;
+
+	case'm':
+		// merge sort
+		break;
+
+	case'q':
+		//quick sort
+		break;
+
+	case'r':
+		// generate new random numbers
+		break;
+
+	case's':
+		//shuffle using fisher-yates algorithm(bonus)
+		break;
+
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key)
 {
-
+	
 }
 
 //--------------------------------------------------------------
@@ -154,3 +154,4 @@ void ofApp::dragEvent(ofDragInfo dragInfo)
 { 
 
 }
+
