518e52a13209384b9f8e5a872a5b9e64945cadd6
vamprock19 <vamprock19@gmail.com>
2025-02-02 23:29:25 -0500

    horizontal circles, random values, number inside the circle

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,6 +2,27 @@
 #include<array>;
 using namespace std;
 
+
+class ofApp : public ofBaseApp
+{
+	void setup();
+	void update();
+	void draw();
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void gotMessage(ofMessage msg);
+	void dragEvent(ofDragInfo dragInfo);
+
+};
+
+
 //--------------------------------------------------------------
 void ofApp::setup()
 {
@@ -30,17 +51,37 @@ void ofApp::draw()
 	/*ofSetColor(255, 255, 0);
 	ofDrawBitmapString("this is the homework!", 100, 100);*/
 	ofSetColor(0, 0, 255);
-	bool isBlue = true;
-	int counter = 200;
+	/*bool isBlue = true;*/
+	
 	srand(time(0));
 	array <int, 5> arrayList = { 0,0,0,0,0 }; /*5*/
+
+	float spacing = ofGetWidth() / 6; 
+	float y = ofGetHeight() / 2;
+
+
 
 
 	for (int i = 0; i < 5; i++)
 	{
-			arrayList[i] = rand() % 100 + 10; //  generates 5 random numbers
-			ofDrawCircle(200, counter, arrayList[i]);// radius
-			counter += 120;
+		//  generates 5 random numbers
+		arrayList[i] = rand() % 100 + 10; 
+		float x = (i + 1) * spacing;
+
+		// radius
+		// prints the circles
+		ofDrawCircle(x, y, arrayList[i]);
+
+		//white color
+		ofSetColor(255,255,255);
+
+		// numbers at the center of the circles
+		// draws the circles
+		ofDrawBitmapString(ofToString(arrayList[i]), x , y );
+		
+		// arranges the color the other circles
+		ofSetColor(0, 0, 255);
+
 	}
 		
 	/*for (int y = 200; y <= 400; y += 100)
@@ -85,7 +126,11 @@ void ofApp::keyPressed(int key)
 		break;
 
 	case'r':
-		// generate new random numbers
+		// regenerate new random numbers
+		for (int i = 0; i < 5; i++)
+		{
+
+		}
 		break;
 
 	case's':
